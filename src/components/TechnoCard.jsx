36d34cb9a0b18ff3083e8d04f07074d57ef4069a
Claire2311 <claire.girard@ieseg.fr>
2023-08-28 13:22:34 +0200

    change image path

--- src/components/TechnoCard.jsx
+++ src/components/TechnoCard.jsx
@@ -3,7 +3,11 @@ function TechnoCard({ name, image }) {
   return (
     <div className="flex  border-gray-300 border-t border-r-lightblue border-r-[80px]">
       <div className="w-14">
-        <img src={`/images/${image}`} className="h-12 py-2 m-auto" />
+        <img
+          src={`public/images/${image}`}
+          alt={name}
+          className="h-12 py-2 m-auto"
+        />
       </div>
       <h1 className="self-center ml-2">{name}</h1>
     </div>
